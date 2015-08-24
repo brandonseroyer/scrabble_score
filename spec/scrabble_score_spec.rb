688accbffba7550d4a60837c50dd41c41787ee67
Brandon Seroyer <brandonseroyer@gmail.com>
2015-08-24 16:12:48 -0700

    final scrabble_score

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,6 +1,13 @@
 require('rspec')
 require('scrabble_score')
+require('pry')
 
-it("returns a scrabble score for a word") do
-  expect("add".scrabble()).to(eq(5))
+describe('String#scrabble') do
+  it("returns a scrabble score for a single letter") do
+    expect("a".scrabble()).to(eq(1))
+  end
+
+  it("returns a scrabble score for a word") do
+    expect("add".scrabble()).to(eq(5))
+  end
 end
