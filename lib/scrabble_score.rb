dfdef1383b620763ed40adfdef833dc80e79b15b
Brandon Seroyer <brandonseroyer@gmail.com>
2016-11-06 01:08:20 -0700

    Update scrabble_score.rb

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,19 +1,14 @@
 class String
-
-  define_method (:scrabble) do
-
+  
+  def scrabble
     letters = {"a" => 1, "e" => 1, "i" => 1, "o" => 1, "u" => 1, "l" => 1, "n" => 1, "r" => 1, "s" => 1, "t" => 1,
       "d" => 2, "g" => 2, "b" => 3, "c" => 3, "m" => 3, "p" => 3, "f" => 4, "h" => 4, "v" => 4, "w" => 4, "y" => 4,
       "k" => 5, "j" => 8, "x" => 8, "q" => 10, "z" => 10}
-
     inputs = self.split("")
     sum = 0
-
     inputs.each() do |input|
       sum = sum + letters.fetch(input)
-      # binding.pry
     end
     sum
   end
-
 end
